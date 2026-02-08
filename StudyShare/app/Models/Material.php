e960b88df8d5651408ec890348710bfc74bfca85
Niko2357 <niki.polach@gmail.com>
2026-02-08 18:51:17 +0100

    first commit

---
+++ StudyShare/app/Models/Material.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Material extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'subject_id',
+        'category_id',
+        'title',
+        'description',
+        'grade_level',
+        'tags',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function subject(): BelongsTo
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function files(): HasMany
+    {
+        return $this->hasMany(File::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+}
